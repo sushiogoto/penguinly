325a10e8f537c02c3e957d37cece827e624cd4ab
sushiogoto <yoshio.goto@gmail.com>
2015-11-09 12:19:54 -0800

    Update lint file and app folders

---
+++ client/app/auth/auth.js
@@ -0,0 +1,35 @@
+// do not tamper with this code in here, study it, but do not touch
+// this Auth controller is responsible for our client side authentication
+// in our signup/signin forms using the injected Auth service
+angular.module('penguinly.auth', ['ngAnimate'])
+
+.controller('AuthController', function ($scope, $window, $location, Auth) {
+  $scope.user = {};
+  $scope.errorBool = false;
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.penguinly', token);
+        $location.path('/groups');
+      })
+      .catch(function (error) {
+        $scope.errorBool = true;
+        $scope.errorMessage = "Username or password incorrect";
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.penguinly', token);
+        $location.path('/groups');
+      })
+      .catch(function (error) {
+        $scope.errorBool = true;
+        $scope.errorMessage = "User already exists";
+        console.error(error);
+      });
+  };
+});
---
+++ client/app/groups/groups.js
@@ -0,0 +1,5 @@
+angular.module('penguinly.groups', [])
+
+.controller('GroupsCtrl', function ($scope) {
+
+});
